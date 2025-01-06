06c1b64859bc954854935f3fc08e711798e0567d
juhee99 <eee6767@naver.com>
2025-01-06 19:16:06 +0900

    feat: eslint 설정 추가
    - favicon 변경

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<div>
-			<h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>
-		</div>
-	);
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainView from './views/MainView';
+import Header from './componets/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainView />
+    </div>
+  );
+}
+
+export default App;
